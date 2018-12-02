c42bab9befaf8787ff8216c2bba50887b0a7c621
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2018-12-02 23:41:45 +0100

    Update Reserve.swift

--- Reserve.swift
+++ Reserve.swift
@@ -8,31 +8,31 @@ protocol ReserveProtocol:Sequence{
     init()
     
 
-    //EstVide: Reserve -> Bool
+    //Est_Vide: Reserve -> Bool
     //Regarde si la reserve est Vide
     //Post: Si la reserve est vide on retourne Vrai sinon on retourne Faux
-    func EstVide()->Bool
+    func Est_Vide()->Bool
 
-    //EstDansReserve: Reserve*Piece -> Bool
+    //Est_Dans_Reserve: Reserve*Piece -> Bool
     //Regarde si il y a une piece dans la reserve
-    //Pre: La piece n’a pas de position (est de type TypePiece*Vide)
+    //Pre: La piece nâ€™a pas de position (est de type TypePiece*Vide)
     //Post: Si la piece est dans la reserve on retourne vrai sinon on retourne faux.
-    func EstDansReserve(type:Piece)->Bool
-    
-    //Ajouter_piece: Reserve*Piece -> Reserve
+    func Est_Dans_Reserve(piece:Piece)->Bool
+     
+    //Ajouter_Piece: Reserve*Piece -> Reserve
     //Ajoute une piece de la reserve et remplace sa position par Vide
     //Pre: La piece a une position
-    //Post: La piece a été ajouté dans la reserve du joueur et on a remplace sa position par vide, elle est maintenant de type TypePiece*Vide. Si jamais la piece n’a pas de postion alors on ne fait rien
+    //Post: La piece a Ã©tÃ© ajoutÃ© dans la reserve du joueur et on a remplace sa position par vide, elle est maintenant de type TypePiece*Vide. Si jamais la piece nâ€™a pas de postion alors on ne fait rien
     @discardableResult
-    mutating func Ajouter_piece(type:Piece)->Self
+    mutating func Ajouter_Piece(piece:Piece)->Self
     
-    //Supprimer_piece: Reserve*Piece -> Reserve
+    //Supprimer_Piece: Reserve*Piece -> Reserve
     //Supprime une Piece de la Reserve
     //Pre: La Piece doit faire partie de la Reserve du joueur
-    //Pre: La piece n’a pas de position (est de type TypePiece*Vide)
-    //Post: La Piece a ete supprimee de la reserve du joueur. Si jamais elle n’en fait pas partie ou si elle n’est pas de type TypePiece*Vide alors on ne fait rien
+    //Pre: La piece nâ€™a pas de position (est de type TypePiece*Vide)
+    //Post: La Piece a ete supprimee de la reserve du joueur. Si jamais elle nâ€™en fait pas partie ou si elle nâ€™est pas de type TypePiece*Vide alors on ne fait rien
     @discardableResult
-    mutating func Supprimer_piece(Type:Piece)->Self
+    mutating func Supprimer_Piece(piece:Piece)->Self
 
     // makeIteratorTypePiece : Reserve -> IteratorTypePiece
     // cree un iterateur sur la collection dans l'odre (?Comment on les tries)
