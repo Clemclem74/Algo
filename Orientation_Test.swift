18b99beb81df2a89554623c89f8bfffbba0616cf
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2018-12-03 10:16:26 +0100

    Update Orientation_Test.swift

--- Orientation_Test.swift
+++ Orientation_Test.swift
@@ -2,17 +2,28 @@ ATTENTION FAUX !!!!
 
 Change_Orientation_Test(){
 	var orientation = Orientation()
-    var type_piece_inexistant = TypePiece() //Crée un type inexistant
-    type_piece_inexistant.Set_Nom("KoKiKo")
-    var ret : Int = 0
-    
-    do {
-        try est_dans_main(main,type_piece_inexistant)
-        print("Test type piece inexistant a echoue")
-        ret+=1
-    }
-    catch {
-        print("OK si type inexistant")
-    }
-    return ret
+	var piece = Piece()
+    	var ret : Int = 0
+  	do {
+        	try Change_Orientation(orientfin : X)
+       		print("Test orientation inexistante a echoue")
+        	ret+=1
+   	}
+    	catch {
+        	print("OK si orientation inexistante")
+    	}
+	
+	orientation.Change_Orientation(orientfin : S)
+	if (orientation == S) {
+		print("OK pour le changement d'orientation")
+	}
+	else {
+		print("ERREUR lors du changement de position")
+		ret+=1
+	}
+	
+    	return ret
 }
+
+var nb_erreur : Int = 0
+nb_erreur+=Change_Orientation_Test()
