c9d636664b8d21c68a473d4500e9e4b8762fbf2e
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2018-12-03 10:48:14 +0100

    Update TypePiece.swift

--- TypePiece.swift
+++ TypePiece.swift
@@ -11,9 +11,5 @@ protocol TypePieceProtocol:{
     //Set_Nom: TypePiece*(String|Vide) -> TypePiece
     //Modifie le nom du type de la piece
     mutating func Set_Nom(nom:String?)->Self
-    
-    //Give_Deplacement: TypePiece -> Deplacements
-    //Donne les deplacements possibles d'un type de piece
-    func Give_Deplacement() -> Deplacements
 }
 
