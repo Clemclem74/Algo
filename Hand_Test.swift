19ae041fcd686d0f60319a61051ad706d179d603
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2018-12-03 09:54:42 +0100

    Update Hand_Test.swift

--- Hand_Test.swift
+++ Hand_Test.swift
@@ -1,15 +1,53 @@
+
+func Initialiser_Debut_Test()->Int {
+    var main = Hand()
+    var type1 = TypePiece() //Crée un type inexistant
+    type1.Set_Nom("koropokkuru")
+    var type1 = TypePiece() //Crée un type inexistant
+    type2.Set_Nom("kitsune")
+    var type1 = TypePiece() //Crée un type inexistant
+    type3.Set_Nom("tanuki")
+    var type1 = TypePiece() //Crée un type inexistant
+    type4.Set_Nom("kodama")
+    var ret : Int = 0
+   
+    main.Initialiser_Debut()
+    if!((main.Est_Dans_Main(type:type1)&&(main.Est_Dans_Main(type:type2))&&(main.Est_Dans_Main(type:type3))&&(main.Est_Dans_Main(type:type1))){
+        print("Test si il y a toute les pieces: Echec")
+        ret+=1
+    }
+    else {
+        print("OK il y a toute les pieces")
+    }
+    return ret
+}
+
+
 func Est_Dans_Main_Test()->Int {
     var main = Hand()
-    var type_piece_inexistant = TypePiece() //Cr�e un type inexistant
-    type_piece_inexistant.Set_Nom("KoKiKo")
+    var type_piece TypePiece()
+    var type_piece_inexistant = TypePiece() //Crée un type inexistant
+    var piece = Piece()
+    type_piece_inexistant.Set_Nom(nom:"KoKiKo")
+    type_piece.Set_Nom(nom:"kodama")
+    piece.Set_Type(type:type_piece)
+    main.Ajouter_Piece(piece:piece,pos:(1,1))
+    
     var ret : Int = 0
     
     do {
-        try est_dans_main(main,type_piece_inexistant)
+        try main.Est_Dans_Main(type:type_piece_inexistant)
         print("Test type piece inexistant a echoue")
         ret+=1
     }
     catch {
+        print("OK si type inexistant")
+    }
+    if !main.Est_Dans_Main(type:type_piece){
+        print("Test type piece si le type existe : ECHEC")
+        ret+=1
+    }
+    else{
         print("OK si type inexistant")
     }
     return ret
@@ -20,21 +58,31 @@ func Est_Dans_Main_Test()->Int {
 func Avoir_Piece_Test()->Int{
     var main = Hand()
     var position_inexistante = Position()
+    var piece = Piece()
+    main.Ajouter_Piece(piece:piece,pos:(1,1))
     position_inexistante.Set_Position(-1,-1)
     var ret : Int = 0
     do {
-        try Avoir_Piece(main:main,positionFinale:position_inexistante)
+        try main.Avoir_Piece(pos:position_inexistante)
         print("Test type piece inexistant a echoue")
         ret+=1
     }
     catch {
         print("OK si type inexistant")
     }
+    if !main.Avoir_Piece(pos:(1,1)){
+        print("Test avoir piece si il y a une piece a cette position : ECHEC")
+        ret+=1
+    }
+    else{
+        print("OK si il y a une piece a cette position")
+    }
     return ret
 }
 
 
-
+    
+    
 
 func Ajouter_Piece_Test()->Int{
     var main = Hand()
@@ -46,7 +94,7 @@ func Ajouter_Piece_Test()->Int{
     var ret : Int = 0
     do {
         try {
-            Ajouter_Piece(main,piece_plateau,(1,1))
+            main.Ajouter_Piece(piece:piece_plateau,pos:(1,1))
             print("Test si une piece qui a une position peut etre ajoute a echoue")
             ret+=1
         }
@@ -56,7 +104,7 @@ func Ajouter_Piece_Test()->Int{
     }
     do {
         try {
-            Ajouter_Piece(main,piece_sans_position,(2,2))
+            main.Ajouter_Piece(piece:piece_sans_position,pos:(2,2))
             print("Test si une piece peut etre ajouter a une position deja occupe a echoue")
             ret+=1
         }
@@ -66,7 +114,7 @@ func Ajouter_Piece_Test()->Int{
     }
     do {
         try {
-            Ajouter_Piece(main,piece_sans_position,(-1,-1))
+            main.Ajouter_Piece(piece:piece_sans_position,pos:(-1,-1))
             print("Test si une piece peut etre ajouter en dehors du plateau a echoue")
             ret+=1
         }
@@ -87,8 +135,8 @@ func Supprimer_Piece_Test()->Int{
     do {
         try {
             //ici on verifie que la piece n'est pas dans la main mais on n'est pas oblige car a auccun moment on a ajouter la piece a la main et la main est initialise a vide donc la piece n'en fait forcement pas partie
-            if !main.Est_Dans_Main(piece_pas_dans_main){
-                Supprimer_Piece(main,piece_plateau,(1,1))
+            if !main.Est_Dans_Main(piece:piece_pas_dans_main){
+                main.Supprimer_Piece(piece:piece_plateau,pos:(1,1))
                 print("Test si une piece qui n'est pas dans la main peut etre supprimer de la main a echoue")
                 ret+=1
             }
