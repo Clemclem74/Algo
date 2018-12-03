6762440a618019896f31e14672f4a73cd45e3187
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2018-12-03 12:21:03 +0100

    Update TypePiece.swift

--- TypePiece.swift
+++ TypePiece.swift
@@ -1,15 +1,18 @@
-//Un typePiece est defini par son nom et ses mouvements possibles
+//Un typePiece est defini par son nom
 protocol TypePieceProtocol:{
     //init -> TypePiece
     //On initialise un TypePiece avec un nom et ses mouvements possibles
     init()
     
-    //Give_Nom: TypePiece -> String
+    //Give_Nom: TypePiece -> (String | Vide)
     //Donne le nom du type de la piece
+    //Post: On a le nom du Type
     func Give_Nom()->String
     
-    //Set_Nom: TypePiece*(String|Vide) -> TypePiece
+    //Set_Nom: TypePiece*String -> TypePiece
     //Modifie le nom du type de la piece
-    mutating func Set_Nom(nom:String?)->Self
+    //Pre: Le nom n'est pas Vide
+    //Post le nom du Type a ete modifie
+    mutating func Set_Nom(nom:String)->Self
 }
 
